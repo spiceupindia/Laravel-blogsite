3500074dc1f776287bdae4a5690d3647099578d4
padma63 <padmavathy.droid01@gmail.com>
2019-11-08 14:04:24 +0530

    PostFactory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,10 +2,10 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Post;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Post::class, function (Faker $faker) {
     return [
         'title' =>$faker->sentence(3),
         'slug' => str_slug('title'),
