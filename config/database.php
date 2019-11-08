641527dc1b5b43d0bb0a28914b0d6fab3ff5cba6
padma63 <padmavathy.droid01@gmail.com>
2019-11-08 13:36:45 +0530

    Add cleardb

--- config/database.php
+++ config/database.php
@@ -1,5 +1,11 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 return [
 
     /*
@@ -42,11 +48,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
+            'host' => $host,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'blogsite'),
-            'username' => env('DB_USERNAME', 'b32e1bf3fef350'),
-            'password' => env('DB_PASSWORD', 'b54572da'),
+            'database' =>  $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
