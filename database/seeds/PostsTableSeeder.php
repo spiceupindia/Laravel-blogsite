5ad18fb81211476a76c5a3c252034109654f1a2d
padma63 <padmavathy.droid01@gmail.com>
2019-11-08 13:57:31 +0530

    Add featured

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -17,7 +17,7 @@ class PostsTableSeeder extends Seeder
             'user_id' => '1',
             'category_id' => '1',
             'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-            'featured' => asset('uploads/posts/'.sample.jpg),
+            'featured' => asset('uploads/posts/sample.jpg'),
         ]);
 
         factory(Post::class,5)->create();
