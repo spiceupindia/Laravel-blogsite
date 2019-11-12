c7fd7c3f67852bd7b8e38babbb55e0c5f6b57e82
padma63 <padmavathy.droid01@gmail.com>
2019-11-12 15:16:31 +0530

    Category name update

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -22,8 +22,8 @@ class FrontendController extends Controller
                     ->with('first_post', Post::orderBy('created_at', 'desc')->first())
                     ->with('second_post', Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first())
                     ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first())
-                    ->with('career', Category::find(5))
-                    ->with('tutorials', Category::find(4));
+                    ->with('laravel', Category::find(1))
+                    ->with('php', Category::find(2));
    }
 
    public function category(Category $category){
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -201,7 +201,7 @@
                     <div class="row">
                         <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
                             <div class="heading">
-                            <h4 class="h1 heading-title">{{ $career->name }}</h4>
+                            <h4 class="h1 heading-title">{{ $laravel->name }}</h4>
                                 <div class="heading-line">
                                     <span class="short-line"></span>
                                     <span class="long-line"></span>
@@ -211,7 +211,7 @@
                     </div>
                     <div class="row">
                         <div class="case-item-wrap">
-                            @foreach($career->posts()->orderBy('created_at', 'desc')->take(3)->get() as $post)
+                            @foreach($laravel->posts()->orderBy('created_at', 'desc')->take(3)->get() as $post)
                                 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                                     <div class="case-item">
                                         <div class="case-item__thumb">
@@ -232,7 +232,7 @@
                     <div class="row">
                         <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
                             <div class="heading">
-                                <h4 class="h1 heading-title">{{ $tutorials->name }}</h4>
+                                <h4 class="h1 heading-title">{{ $php->name }}</h4>
                                 <div class="heading-line">
                                     <span class="short-line"></span>
                                     <span class="long-line"></span>
@@ -242,7 +242,7 @@
                     </div>
                     <div class="row">
                         <div class="case-item-wrap">
-                            @foreach($tutorials->posts()->orderBy('created_at','desc')->take(3)->get() as $post)
+                            @foreach($php->posts()->orderBy('created_at','desc')->take(3)->get() as $post)
                                 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                                     <div class="case-item">
                                         <div class="case-item__thumb">
