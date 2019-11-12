764c6e8b9452aae0db8bdc8bc60543f799b017d0
padma63 <padmavathy.droid01@gmail.com>
2019-11-12 15:59:40 +0530

    add secure asset for js

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -96,17 +96,17 @@
 
 <!-- JS Script -->
 
-<script src="{{ asset('app/js/jquery-2.1.4.min.js')}}"></script>
-<script src="{{ asset('app/js/crum-mega-menu.js')}}"></script>
-<script src="{{ asset('app/js/swiper.jquery.min.js')}}"></script>
-<script src="{{ asset('app/js/theme-plugins.js')}}"></script>
-<script src="{{ asset('app/js/main.js')}}"></script>
-<script src="{{ asset('app/js/form-actions.js')}}"></script>
+<script src="{{ secure_asset('app/js/jquery-2.1.4.min.js')}}"></script>
+<script src="{{ secure_asset('app/js/crum-mega-menu.js')}}"></script>
+<script src="{{ secure_asset('app/js/swiper.jquery.min.js')}}"></script>
+<script src="{{ secure_asset('app/js/theme-plugins.js')}}"></script>
+<script src="{{ secure_asset('app/js/main.js')}}"></script>
+<script src="{{ secure_asset('app/js/form-actions.js')}}"></script>
 
-<script src="{{ asset('app/js/velocity.min.js')}}"></script>
-<script src="{{ asset('app/js/ScrollMagic.min.js')}}"></script>
-<script src="{{ asset('app/js/animation.velocity.min.js')}}"></script>
-<script src="{{ asset('js/toastr.min.js')}}"></script>
+<script src="{{ secure_asset('app/js/velocity.min.js')}}"></script>
+<script src="{{ secure_asset('app/js/ScrollMagic.min.js')}}"></script>
+<script src="{{ secure_asset('app/js/animation.velocity.min.js')}}"></script>
+<script src="{{ secure_asset('js/toastr.min.js')}}"></script>
 <script>
         @if(Session::has('success'))
             toastr.success("{{ Session::get('success') }}")
