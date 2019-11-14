bfbcf936ddbce18e628cad8073e23c4e04493464
padma63 <padmavathy.droid01@gmail.com>
2019-11-14 13:52:21 +0530

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(UsersTableSeeder::class);
-         //$this->call(PostsTableSeeder::class);
-         //$this->call(SettingsTableSeeder::class);
+         $this->call(PostsTableSeeder::class);
+         $this->call(SettingsTableSeeder::class);
          $this->call(CategoryTableSeeder::class);
          $this->call(TagTableSeeder::class);
     }
