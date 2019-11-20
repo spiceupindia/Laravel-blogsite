a4c016ae6eccb469ac1a19ec6f896c757dd3cd76
padmavathy-mca <padmavathy6.mca@gmail.com>
2019-11-20 11:04:43 +0530

    Httpsprotocol

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -51,8 +51,8 @@ return [
     //    Illuminate\Http\Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
      //   Illuminate\Http\Request::HEADER_CLIENT_HOST  => null, // not set on AWS or Heroku
 //        Illuminate\Http\Request::HEADER_CLIENT_HOST  => 'X_FORWARDED_HOST',
-        Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
-        Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
+     //   Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
+     //   Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
     ]
 
 ];
