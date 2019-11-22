ed6057a1e2910de7f462b779f38cc367e21562bb
padmavathy-mca <padmavathy6.mca@gmail.com>
2019-11-22 11:55:38 +0530

    Settings

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -33,18 +33,20 @@ class SettingsController extends Controller
 
        $settings = Setting::first();
 
-       $settings->site_name = request()->site_name;
+      /* $settings->site_name = request()->site_name;
        $settings->contact_number = request()->contact_number;
        $settings->contact_email = request()->contact_email;
        $settings->address = request()->address;
-       
-       $settings->save();
-        
+
+       $settings->save(); */
+
+       $settings->fill($request->input())->save();
+
        Session::flash('success', 'Settings Updated successfully');
 
        return redirect()->back();
-   
+
     }
 
-    
+
 }
