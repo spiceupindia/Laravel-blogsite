25c68cf8895d83562a11e1749c3da0a6f8f292f0
padmavathy-mca <padmavathy6.mca@gmail.com>
2019-11-26 12:27:26 +0530

    show posts

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,32 +1,26 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="card">
-        <div class="card-header"> <b>{{$post->title}}</b></div>
+<div class="card">
+    <div class="card-body">
 
-        <div class="card-body">
-             <table class="table table-hover">
-                
-                          <tr>
-                              <td class="text-center">
-                                  <img class="rounded mx-auto d-block" src="{{ $post->featured }}" alt="{{$post->title}}" width="100%" height="250px">
-                              </td>
-                          </tr>
-                          <tr>
-                                <td>
-                                    {!!$post->content!!}
-                                </td> 
-                            </tr>   
-        
-                             <tr>
-                                <td>
-                                    <a href="{{ route('post.edit',['id'=> $post->id ]) }}" class="btn btn-sm btn-info">Edit</a>
-                                </td>
-                                <td>
-                                    <a href="{{ route('post.delete',['id' => $post->id ])}}" class="btn btn-sm btn-warning">Trash</a>
-                                </td>
-                          </tr>                  
-             </table>
-        </div>
+      <h5 class="card-title">Show Details: {{ $post->title }}</h5>
+      <img src="{{ asset('uploads/posts/'.$post->featured_image) }}" alt="{{ $post->title }}" height="250px" width="100%">
+      <p class="card-text">{!! $post->content !!}</p>
+      <p>Category: {{ $post->category->name }}</p>
+      <p>Created By: {{ $post->user->name }}</p>
+      <div class="d-flex justify-content-between">
+            <a href="{{ route('posts.edit',['id' => $post->id ])}}" class="btn btn-primary"><i class="fas fa-edit"></i></a>
+            <form action="{{ route('posts.destroy',['id' => $post->id ])}}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger"><i class="fas fa-trash"></i></button>
+            </form>
+      </div>
+
+
     </div>
+  </div>
+
+
 @endsection
