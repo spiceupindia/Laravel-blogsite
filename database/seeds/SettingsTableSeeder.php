fcd95061a9534fe65df04d5f2a423c19c522ac51
padmavathy-mca <padmavathy6.mca@gmail.com>
2019-11-26 10:35:46 +0530

    Settings

--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -12,7 +12,7 @@ class SettingsTableSeeder extends Seeder
     public function run()
     {
         \App\Setting::create([
-            'site_name' => "Laravel's Blog",          
+            'site_name' => "Laravel Blog",
             'contact_number' => '91 9562 6874 63',
             'contact_email' => 'info@laravel_blog.com',
             'address' => 'Russia, Petersburg',
